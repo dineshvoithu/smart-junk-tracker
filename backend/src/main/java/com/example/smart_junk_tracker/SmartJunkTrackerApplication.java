ae093c51925c9d86fbf782cc6541b19d885a4989
dineshvoithu <dineshkumarvoithu@gmail.com>
2025-09-20 21:04:36 +0530

    Initial commit for backend

---
+++ backend/src/main/java/com/example/smart_junk_tracker/SmartJunkTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_junk_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJunkTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartJunkTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/smart_junk_tracker/SmartJunkTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_junk_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartJunkTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
